3327f28efc9e2c3ee19feef6aacee530d7e9e624
yuzhanglong <loveyzl1123@gmail.com>
2023-07-04 10:44:31 +0800

    chore: update config

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log('script execute');
-throw new Error('hello world!');
+console.log('cross-origin script execute...');
+
+
+setTimeout(() => {
+  const a = {};
+  console.log(a.b.c);
+}, 1000);
